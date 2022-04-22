de9cb49b4b75ab45bbc22fd17132b914e945b7fc
Barbaros YILDIRIM <barbarosayildirim@gmail.com>
2022-04-22 16:39:31 +0300

    chore: date util

---
+++ src/utils/date.js
@@ -0,0 +1,11 @@
+export default function FormatDate(dateString) {
+  if (!dateString) return "";
+  const date = new Date(dateString);
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+  return date.toLocaleDateString("en-US", options);
+}
