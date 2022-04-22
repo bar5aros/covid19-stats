f94b13fac709a5d5dd404ebb50fcca7c68496b99
Barbaros YILDIRIM <barbarosayildirim@gmail.com>
2022-04-22 17:16:50 +0300

    fix: package fixes

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import Details from "./pages/Details";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
