a8a471e43f3163f3c388b1ab7cd23c636ca8d7a5
Barbaros YILDIRIM <barbarosayildirim@gmail.com>
2022-04-22 17:05:38 +0300

    feat: add typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,53 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {
-    // code style
-    semi: ["error", "always"],
-    quotes: ["error", "double"],
-    // suppress errors for missing 'import React' in files
-    "react/react-in-jsx-scope": "off",
-    // allow jsx syntax in js files (for next.js project)
-    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
-    // error importing stuff
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        js: "never",
-        jsx: "never",
-        ts: "never",
-        tsx: "never",
-      },
+    extends: [
+        "plugin:react/recommended",
+        "airbnb",
+        "airbnb-typescript",
+        "prettier",
     ],
-    // we'll migrate using typescript, just disabling this
-    "react/prop-types": "off",
-    "react/destructuring-assignment": "off",
-  },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        project: "./tsconfig.json",
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    plugins: ["react", "@typescript-eslint"],
+    rules: {
+        // code style
+        semi: ["error", "always"],
+        quotes: ["error", "double"],
+        // suppress errors for missing 'import React' in files
+        "react/react-in-jsx-scope": "off",
+        // allow jsx syntax in js files (for next.js project)
+        "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+        // error importing stuff
+        "import/extensions": [
+            "error",
+            "ignorePackages",
+            {
+                js: "never",
+                jsx: "never",
+                ts: "never",
+                tsx: "never",
+            },
+        ],
+        // we'll migrate using typescript, just disabling this
+        "react/prop-types": "off",
+        "react/destructuring-assignment": "off",
+        "react/jsx-filename-extension": [
+            2,
+            { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+        ],
+        "react/react-in-jsx-scope": "off",
+        "no-shadow": "off",
+        semi: ["error", "always"],
+        "import/no-unresolved": "off",
+    },
 };
